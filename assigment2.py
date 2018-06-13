c41b8e0127d32d208009b33d2b469982e666c096
unknown <you@example.com>
2018-06-13 15:57:58 +0530

    assignment submited

---
+++ assigment2.py
@@ -0,0 +1,33 @@
+#print anything you want on screen.
+print("learing python")
+print("institute acadview")
+print("place-Gnit,Mullana")
+
+# Join two string using'+'
+x="acad"
+y="view"
+z=x+y
+print(z)
+  
+ #take the input of 3 variable x,y,z. print their values on screen
+x=int(input("enter the value of x: "))
+y=int(input("enter the value of y: "))   
+z=x+y
+print(z)
+
+#print("let's get started ")on screen
+
+print('"let\'s get started"')
+
+#print placeholder
+s="acadview"
+course="python"
+fees=4000
+print('%s %s %d' %(s,course,fees))
+
+
+
+#exercise
+name=("tony shark")
+salary=1000000
+print('%s %d'%(name,salary))
---
+++ assignment3.py
@@ -0,0 +1,14 @@
+#Q1:Creat a list with use-defind inputs.
+
+#Q2:Add the following list to above created list:
+#         ['google','apple','microsoft','tesla']
+
+
+
+#Q3:Count the number of time an object occurs in the list.
+
+
+#Creat the number of list and short this in ascending order.
+
+
+l=[5,2,1,4,3]
